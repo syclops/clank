fa421ed1aa39bb3e00814cd1f2a9b2055c5909e7
Steve Matsumoto <steve@stevematsumoto.net>
2015-06-07 14:43:26 +0200

    Added skeleton of main Clank shell

---
+++ src/clank.py
@@ -0,0 +1,21 @@
+# Clank Main Shell
+# Author: Steve Matsumoto
+# This code is released under the GNU LGPL Version 3.
+
+"""
+:mod:`clank` --- Main Clank Shell
+=================================
+"""
+
+import cmd
+
+class Shell(cmd.Cmd):
+    """
+    Command-line interface to receive and process user commands.
+    """
+
+    intro = 'Clank Version 0.1 - Steve Matsumoto\n'
+    prompt = 'clank > '
+
+if __name__ == '__main__':
+    Shell().cmdloop()
