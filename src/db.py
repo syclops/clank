1b9f9b664c049260885331c83fd8111097454794
Steve Matsumoto <steve@stevematsumoto.net>
2015-06-08 23:37:04 +0200

    Added skeleton database module

---
+++ src/db.py
@@ -0,0 +1,21 @@
+# Clank Database
+# Author: Steve Matsumoto
+# This code is released under the GNU LGPL Version 3.
+
+"""
+:mod:`db` --- Clank Database
+============================
+"""
+
+import sqlite3
+
+class DB(object):
+    """
+    Database for Clank.
+    """
+
+    def __init__(self):
+        """
+        Constructor.
+        """
+        pass
