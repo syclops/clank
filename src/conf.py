b27361d2cd5296d20819b40ba31107d29d90de6e
Steve Matsumoto <steve@stevematsumoto.net>
2015-06-08 23:52:30 +0200

    Added skeleton configuration file

---
+++ src/conf.py
@@ -0,0 +1,21 @@
+# Clank Configuration Manager
+# Author: Steve Matsumoto
+# This code is released under the GNU LGPL Version 3.
+
+"""
+:mod:`conf` --- Clank Configuration Manager
+===========================================
+"""
+
+import configparser
+
+class Config(object):
+    """
+    Representation of the Clank session configuration.
+    """
+
+    def __init__(self, configFile=None):
+        """
+        Constructor.
+        """
+        pass
